60f7bd0750070c14aa7377b73b8e8101e849346d
asvool <asvool1990@gmail.com>
2024-04-02 22:37:41 +0300

    Update LICENSE

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         User user1 = new User("Ivan", 33, "man", 3, "online");
+        User user2 =new User("Dima", 20, "man",1, "online");
 
-        System.out.println(user1.getName()+" "+user1.getAge()+" "+user1.getGender());
         user1.info();
+        user2.info();
     }
 }
